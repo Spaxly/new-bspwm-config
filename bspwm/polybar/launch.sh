124253ad82b5752ad7378212b306c607e5664962
ohm <ohmrc2545@gmail.com>
2022-07-02 18:02:15 -0400

    major polybar changes

--- bspwm/polybar/launch.sh
+++ bspwm/polybar/launch.sh
@@ -1,4 +1,7 @@
 #!/bin/bash
 
 killall polybar
-polybar -c ~/.config/bspwm/polybar/config.ini
+polybar workspace -c ~/.config/bspwm/polybar/config.ini &
+polybar windowbar -c ~/.config/bspwm/polybar/config.ini &
+polybar spotifymodule -c ~/.config/bspwm/polybar/config.ini & 
+polybar othermodules -c ~/.config/bspwm/polybar/config.ini &
