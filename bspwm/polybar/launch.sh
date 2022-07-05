acccf14414a4c10b217422545bbe61838fc9b063
ohm <ohmrc2545@gmail.com>
2022-07-04 23:56:39 -0400

    changes

--- bspwm/polybar/launch.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-killall polybar
-polybar workspace -c ~/.config/bspwm/polybar/config.ini &
-polybar windowbar -c ~/.config/bspwm/polybar/config.ini &
-polybar spotifymodule -c ~/.config/bspwm/polybar/config.ini & 
-polybar othermodules -c ~/.config/bspwm/polybar/config.ini &
-polybar applauncher -c ~/.config/bspwm/polybar/config.ini &
--- config/bspwm/polybar/launch.sh
+++ config/bspwm/polybar/launch.sh
@@ -1,4 +1,8 @@
 #!/bin/bash
 
 killall polybar
-polybar -c ~/.config/bspwm/polybar/config.ini
+polybar workspace -c ~/.config/bspwm/polybar/config.ini &
+polybar windowbar -c ~/.config/bspwm/polybar/config.ini &
+polybar spotifymodule -c ~/.config/bspwm/polybar/config.ini & 
+polybar othermodules -c ~/.config/bspwm/polybar/config.ini &
+polybar applauncher -c ~/.config/bspwm/polybar/config.ini &
